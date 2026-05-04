6b3ba63abc94069616910ad246cb8ebed9c1d5df
Eirik Andre Lindseth <eirikli@uia.no>
2026-05-04 18:49:01 +0200

    fix run.sh: remove unsupported zoom-to-fit on Linux

--- run.sh
+++ run.sh
@@ -13,6 +13,5 @@ if [[ "$(uname)" == "Darwin" ]]; then
         -display cocoa,zoom-to-fit=on
 else
     qemu-system-i386 -boot d -cdrom "$ISO" -m 64 \
-        -audiodev sdl,id=audio0,out.buffer-length=40000 -machine pcspk-audiodev=audio0 \
-        -display sdl,zoom-to-fit=on
+        -audiodev sdl,id=audio0,out.buffer-length=40000 -machine pcspk-audiodev=audio0
 fi
